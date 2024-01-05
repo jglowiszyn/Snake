76a3cb6b50cfd3c74aa241c371d3caab9e018da8
jglowiszyn <jglowiszyn@wp.pl>
2024-01-05 18:57:10 +0100

    End of the project.

--- src/Snake.java
+++ src/Snake.java
@@ -1,2 +1,19 @@
-public class Snake {
+import javax.swing.*;
+
+ public class Snake extends JFrame{
+
+    Snake(){
+        add(new Board());
+        pack();
+
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setTitle("Snake Game");
+
+    };
+
+    public static void main(String[] args) {
+        new Snake().setVisible(true);
+    }
+
 }
